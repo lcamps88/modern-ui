e3446c8fa21525b7883306db586b7c8c28e1b414
Lisandra <63977965+lcamps88@users.noreply.github.com>
2022-02-10 12:39:18 -0500

    design complete

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import possibility from '../../assets/possibility.png'
+import './possibility.css'
+
+const Possibility = () => {
+  return (
+    <div className="gpt3__possibility section__padding">
+      <di className="gpt3_possibility-image">
+        <img src={possibility} alt="possibility" />
+      </di>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Possibility
